de7ba7b97291778a707b9c0b4c09d58d4cccaa34
kwansai724 <kwansai.taka724@gmail.com>
2021-12-29 10:38:57 +0900

    先生一覧アイコン修正

--- pages/teacher_index.vue
+++ pages/teacher_index.vue
@@ -1,61 +1,79 @@
 <template>
   <v-app>
     <h1>先生一覧</h1>
-    <v-card
-      class="mx-auto"
-      max-width="300"
+    <v-dialog
+      v-model="dialog"
+      width="500"
     >
-      <v-img
-        v-bind:src="image_src"
-      />
+      <template #activator="{ on, attrs }">
+        <v-card
+          class="mx-auto"
+          max-width="250"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-img
+            :src="image_src"
+          />
 
-      <v-card-subtitle class="pb-0">
-        〇〇先生
-      </v-card-subtitle>
-
-      <v-card-text class="text--primary">
-        <div>国語</div>
-      </v-card-text>
-
-      <v-dialog
-        v-model="dialog"
-        width="500"
-      >
-        <template #activator="{ on, attrs }">
-          <v-btn
-            color="orange"
-            text
-            v-bind="attrs"
-            v-on="on"
-          >
-            詳細
-          </v-btn>
-        </template>
-
-        <v-card>
-          <v-card-title class="text-h5 grey lighten-2">
-            Privacy Policy
+          <v-card-title class="text-h5 pt-0 pb-4 justify-center">
+            〇〇先生
           </v-card-title>
-
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </v-card-text>
-
-          <v-divider />
-
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
-            >
-              閉じる
-            </v-btn>
-          </v-card-actions>
         </v-card>
-      </v-dialog>
-    </v-card>
+      </template>
+
+      <v-card>
+        <v-row
+          align="end"
+          class="fill-height"
+        >
+          <v-col
+            align-self="start"
+            class="pa-0"
+            cols="12"
+          >
+            <v-avatar
+              class="profile"
+              color="grey"
+              size="164"
+              tile
+            >
+              <v-img
+                :src="image_src"
+              />
+            </v-avatar>
+          </v-col>
+          <v-col class="py-0">
+            <v-list-item
+              color="rgba(0, 0, 0, .4)"
+              dark
+            >
+              <v-list-item-content>
+                <v-list-item-title
+                  class="text-h6"
+                >
+                  ◯◯先生
+                </v-list-item-title>
+                <v-list-item-subtitle>国語</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+        </v-row>
+
+        <v-divider />
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            閉じる
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
