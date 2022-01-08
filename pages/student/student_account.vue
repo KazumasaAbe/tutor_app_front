8be5a4b49b9a1fc02b9b0f6efa165084a1766830
AbeKazumasa <katahira0088@gmail.com>
2022-01-09 00:17:52 +0900

    トライ

--- pages/student/student_account.vue
+++ pages/student/student_account.vue
@@ -1,25 +1,35 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          生徒マイページ
-        </v-card-title>
-        <v-card-actions>
-          <v-btn
-            @click="check()"
-          >
-            確認
-          </v-btn>
-          <v-spacer />
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-main>
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6">
+          <v-card>
+            <v-card-title class="headline">
+              生徒マイページ
+            </v-card-title>
+            <v-card-actions>
+              <v-btn
+                @click="check()"
+              >
+                確認
+              </v-btn>
+              <v-spacer />
+              <Chart />
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
+import Chart from '~/components/Chart.vue'
+
 export default {
+  components: {
+    Chart
+  },
   data () {
     return {
       abilities: []
