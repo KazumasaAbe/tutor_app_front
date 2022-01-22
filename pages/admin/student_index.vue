52b5b476e3c52bb1484509f08d2a48d44d17bbc1
kwansai724 <kwansai.taka724@gmail.com>
2022-01-22 14:26:29 +0900

    先生、生徒一覧のSearch欄の調整

--- pages/admin/student_index.vue
+++ pages/admin/student_index.vue
@@ -3,35 +3,18 @@
     <h2 class="mt-2">
       生徒一覧
     </h2>
-    <v-card
-      class="mt-5"
+    <v-data-table
+      :headers="headers"
+      :items="students"
+      :items-per-page="5"
+      :search="search"
+      class="elevation-1 text-h6 mt-3"
     >
-      <v-toolbar
-        flat
-      >
-        <v-card-title
-          class="ml-auto"
+      <template #top>
+        <v-toolbar
+          flat
         >
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          />
-        </v-card-title>
-      </v-toolbar>
-      <v-data-table
-        :headers="headers"
-        :items="students"
-        :items-per-page="5"
-        :search="search"
-        class="elevation-1 text-h6"
-      >
-        <template #top>
-          <v-toolbar
-            flat
-          >
+          <v-toolbar-title>
             <v-dialog
               v-model="dialog"
               max-width="600px"
@@ -40,13 +23,33 @@
                 <v-btn
                   color="primary"
                   dark
-                  class="mb-2"
+                  class="mb-2 mt-2"
                   v-bind="attrs"
                   v-on="on"
                 >
                   生徒追加
                 </v-btn>
               </template>
+            </v-dialog>
+          </v-toolbar-title>
+          <v-spacer />
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          />
+          <v-dialog
+            v-model="dialog"
+            max-width="600px"
+          >
+            <v-card
+              class="mt-5"
+            >
+              <v-card-title
+                class="ml-auto"
+              />
               <v-card>
                 <v-row>
                   <v-col
@@ -201,13 +204,12 @@
                   <v-col>
                     <v-card-actions>
                       <v-spacer />
-                      <v-btn
-                        outlined
-                        small
-                        @click="dialog = false"
+                      <v-icon
+                        class="mt-n4 mr-3"
+                        @click="close"
                       >
-                        ✖️
-                      </v-btn>
+                        mdi-close-box-outline
+                      </v-icon>
                     </v-card-actions>
                   </v-col>
                 </v-row>
@@ -227,44 +229,44 @@
                   </v-col>
                 </v-card-actions>
               </v-card>
-            </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5">
-                  この生徒を削除します。よろしいですか？
-                </v-card-title>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn color="red darken-1" text @click="closeDelete">
-                    いいえ
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">
-                    はい
-                  </v-btn>
-                  <v-spacer />
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </template>
-        <template #[`item.actions`]="{ item }">
-          <v-icon
-            class="mr-1"
-            color="blue"
-            @click="showItem(item)"
-          >
-            mdi-pencil
-          </v-icon>
-          <v-icon
-            class=""
-            color="red"
-            @click="deleteItem(item)"
-          >
-            mdi-delete
-          </v-icon>
-        </template>
-      </v-data-table>
-    </v-card>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">
+                この生徒を削除します。よろしいですか？
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="red darken-1" text @click="closeDelete">
+                  いいえ
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">
+                  はい
+                </v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template #[`item.actions`]="{ item }">
+        <v-icon
+          class="mr-1"
+          color="blue"
+          @click="showItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          class=""
+          color="red"
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
   </v-app>
 </template>
 
--- pages/admin_teacher_index.vue
+++ pages/admin_teacher_index.vue
@@ -3,35 +3,18 @@
     <h2 class="mt-2">
       先生一覧
     </h2>
-    <v-card
-      class="mt-5"
+    <v-data-table
+      :headers="headers"
+      :items="teachers"
+      :items-per-page="5"
+      :search="search"
+      class="elevation-1 text-h6 mt-3"
     >
-      <v-toolbar
-        flat
-      >
-        <v-card-title
-          class="ml-auto"
+      <template #top>
+        <v-toolbar
+          flat
         >
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          />
-        </v-card-title>
-      </v-toolbar>
-      <v-data-table
-        :headers="headers"
-        :items="teachers"
-        :items-per-page="5"
-        :search="search"
-        class="elevation-1 text-h6"
-      >
-        <template #top>
-          <v-toolbar
-            flat
-          >
+          <v-toolbar-title>
             <v-dialog
               v-model="dialog"
               max-width="700px"
@@ -40,13 +23,33 @@
                 <v-btn
                   color="primary"
                   dark
-                  class="mb-2"
+                  class="mb-2 mt-2"
                   v-bind="attrs"
                   v-on="on"
                 >
                   先生追加
                 </v-btn>
               </template>
+            </v-dialog>
+          </v-toolbar-title>
+          <v-spacer />
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          />
+          <v-dialog
+            v-model="dialog"
+            max-width="700px"
+          >
+            <v-card
+              class="mt-5"
+            >
+              <v-card-title
+                class="ml-auto"
+              />
               <v-card>
                 <v-row>
                   <v-col>
@@ -76,7 +79,7 @@
                       </v-list-item-content>
                     </v-list-item>
                     <v-list-item
-                      class="ml-5"
+                      class="ml-5 mt-n5"
                     >
                       <v-list-item-content>
                         <v-list-item-subtitle>メールアドレス</v-list-item-subtitle>
@@ -87,7 +90,7 @@
                       </v-list-item-content>
                     </v-list-item>
                     <v-list-item
-                      class="ml-5"
+                      class="ml-5 mt-n5"
                     >
                       <v-list-item-content>
                         <v-list-item-subtitle>担当教科</v-list-item-subtitle>
@@ -101,13 +104,12 @@
                   <v-col>
                     <v-card-actions>
                       <v-spacer />
-                      <v-btn
-                        outlined
-                        small
-                        @click="dialog = false"
+                      <v-icon
+                        class="mt-n4 mr-3"
+                        @click="close"
                       >
-                        ✖️
-                      </v-btn>
+                        mdi-close-box-outline
+                      </v-icon>
                     </v-card-actions>
                   </v-col>
                 </v-row>
@@ -141,43 +143,43 @@
                   </v-btn>
                 </v-card-actions>
               </v-card>
-            </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5">
-                  この先生を削除します。よろしいですか？
-                </v-card-title>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn color="red darken-1" text @click="closeDelete">
-                    いいえ
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">
-                    はい
-                  </v-btn>
-                  <v-spacer />
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </template>
-        <template #[`item.actions`]="{ item }">
-          <v-icon
-            class="mr-1"
-            color="blue"
-            @click="showItem(item)"
-          >
-            mdi-pencil
-          </v-icon>
-          <v-icon
-            color="red"
-            @click="deleteItem(item)"
-          >
-            mdi-delete
-          </v-icon>
-        </template>
-      </v-data-table>
-    </v-card>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">
+                この先生を削除します。よろしいですか？
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="red darken-1" text @click="closeDelete">
+                  いいえ
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">
+                  はい
+                </v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template #[`item.actions`]="{ item }">
+        <v-icon
+          class="mr-1"
+          color="blue"
+          @click="showItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          color="red"
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
   </v-app>
 </template>
 
